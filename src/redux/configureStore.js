f05fbad81ddf98a1c9862a4c199aa173234ab08b
Emily Dardis <63334996+EmilyGitHub@users.noreply.github.com>
2020-06-13 02:24:12 -0700

    React Redux Form Validation

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
 import { createStore } from 'redux';
-import { Reducer, intialState } from './reducer';
+import { Reducer, initialState } from './reducer';
 
 export const ConfigureStore = () => {
     const store = createStore(
         Reducer,
-        intialState
+        initialState
     );
 
     return store;
