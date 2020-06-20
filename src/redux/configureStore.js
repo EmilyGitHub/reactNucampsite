788418b08e369b5918dded143eb16f9be8a840a0
Emily Dardis <63334996+EmilyGitHub@users.noreply.github.com>
2020-06-20 02:59:44 -0700

    React Redux Forms Revisted

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,9 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
+import { InitialFeedback } from './forms';
 import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -12,8 +14,11 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
-        }),
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
+        }),       
         applyMiddleware(thunk, logger)
     );
 
